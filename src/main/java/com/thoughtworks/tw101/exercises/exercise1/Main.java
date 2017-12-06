c615a5b4eddb6e4fe9f6b30d532c726c0f5abd09
Jackie Robinson <jackierobinsonm@gmail.com>
2017-12-06 15:26:01 -0600

    Fixed Exercise 1 and Finished Exercise 3

--- src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise1/Main.java
@@ -4,9 +4,11 @@ package com.thoughtworks.tw101.exercises.exercise1;
 
 public class Main {
     public static void main(String[] args) {
+        int sum = 0;
         for (int i = 1; i < 100; i = i+2) {
+            sum += i;
             System.out.print(i + " ");
         }
-
+        System.out.printf("%n %d", sum);
     }
 }
--- src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise3/OddSum.java
@@ -3,6 +3,10 @@ package com.thoughtworks.tw101.exercises.exercise3;
 public class OddSum {
 
     public int of(int start, int end) {
-        return -1;
+        int sum = 0;
+        for (int i = start; i <= end; i = i+2 ) {
+            sum += i;
+        }
+        return sum;
     }
 }
