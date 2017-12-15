a2d35017e041e6ad53599ccaa5da5fb7dfef8a8a
Jackie Robinson <jackierobinsonm@gmail.com>
2017-12-14 19:35:26 -0600

    fixed exercise 7 finished exercise 8
    changed exercise 7 to use Interger.ParseInt

--- src/main/java/com/thoughtworks/tw101/exercises/exercise8/Game.java
+++ src/main/java/com/thoughtworks/tw101/exercises/exercise8/Game.java
@@ -1,5 +1,8 @@
-package com.thoughtworks.tw101.exercises.exercise7;
+package com.thoughtworks.tw101.exercises.exercise8;
 
+import com.thoughtworks.tw101.exercises.exercise7.GameDisplayText;
+
+import java.util.ArrayList;
 import java.util.Random;
 import java.util.Scanner;
 
@@ -7,22 +10,35 @@ public class Game {
 
     private int randomNumber;
     private int guess;
+    private ArrayList<Integer> guesses;
     private Scanner scan = new Scanner(System.in);
 
     public Game() {
         Random rand = new Random();
         randomNumber = rand.nextInt(100) + 1;
+        guesses = new ArrayList<>();
     }
 
     public void start(){
         displayGameText(GameDisplayText.instructions);
-        guess = Integer.parseInt(scan.next());
+        getNextGuess();
 
         while(!guessCorrect()){
             analyzeGuess(guess);
-            guess = Integer.parseInt(scan.next());
+            getNextGuess();
         }
         displayGameText(GameDisplayText.win);
+        displayGuesses();
+    }
+
+    public void getNextGuess() {
+        try{
+            guess = Integer.parseInt(scan.next());
+            guesses.add(guess);
+        }catch (NumberFormatException e){
+            System.out.println("Please enter an integer");
+            getNextGuess();
+        }
     }
 
     private void analyzeGuess(int guess) {
@@ -30,6 +46,12 @@ public class Game {
             displayGameText(GameDisplayText.guessTooLow);
         else
             displayGameText(GameDisplayText.guessTooHigh);
+    }
+
+    private void displayGuesses(){
+        for (int guess: guesses) {
+            System.out.print(guess + " ");
+        }
     }
 
     private boolean guessCorrect(){
