7374883bbabf47ee60cf9f4b939889ae8e23e3d3
Jackie Robinson <jackierobinsonm@gmail.com>
2017-12-15 22:51:30 -0600

    finished diamond exercises

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/DiamondExercises.java
@@ -13,7 +13,19 @@ public class DiamondExercises {
 //             ***
 //            *****
     private static void drawAnIsoscelesTriangle(int n) {
-
+        int totalRows = n;
+        int leftPadding = n-1;
+        int totalAsterisks = 1;
+        for(int i = 1; i <= totalRows; i++){
+            for(int j = leftPadding; j >0; j--)
+                System.out.print(" ");
+            for(int j = 1 ; j <= totalAsterisks; j++){
+                System.out.print("*");
+            }
+            leftPadding--;
+            System.out.println();
+            totalAsterisks = totalAsterisks + 2;
+        }
     }
 
 //    Diamond
@@ -24,7 +36,26 @@ public class DiamondExercises {
 //             ***
 //              *
     private static void drawADiamond(int n) {
-
+        int totalRows = n + (n-1);
+        int totalAsterisks = 1;
+        int leftPadding = n-1;
+        for(int i = 1; i <= totalRows; i++){
+            for(int j = leftPadding; j >0; j--)
+                System.out.print(" ");
+            for(int j = 1 ; j <= totalAsterisks; j++){
+                System.out.print("*");
+            }
+            System.out.println();
+            if(i >= n)
+            {
+                leftPadding++;
+                totalAsterisks = totalAsterisks -2;
+            }
+            else{
+                leftPadding--;
+                totalAsterisks = totalAsterisks + 2;
+            }
+        }
     }
 
 //    Diamond with Name
@@ -36,6 +67,30 @@ public class DiamondExercises {
 //            ***
 //             *
     private static void drawADiamondWithYourName(int n) {
+        int totalRows = n + (n-1);
+        int totalAsterisks = 1;
+        int leftPadding = n-1;
+        for(int i = 1; i <= totalRows; i++) {
+            if (i != n) {
+                for (int j = leftPadding; j > 0; j--)
+                    System.out.print(" ");
 
+                for (int j = 1; j <= totalAsterisks; j++) {
+                System.out.print("*");
+                }
+            }
+            else
+                System.out.print("Jackie");
+            System.out.println();
+            if(i >= n)
+            {
+                leftPadding++;
+                totalAsterisks = totalAsterisks -2;
+            }
+            else{
+                leftPadding--;
+                totalAsterisks = totalAsterisks + 2;
+            }
+        }
     }
 }
