e8451d9ee8365b04f505e15924f8448e7eed38b4
Jackie Robinson <jackierobinsonm@gmail.com>
2017-12-16 00:10:51 -0600

    Finished Prime Factors

--- src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
+++ src/main/java/com/thoughtworks/tw101/introductory_programming_exercises/PrimeFactors.java
@@ -6,6 +6,7 @@ package com.thoughtworks.tw101.introductory_programming_exercises;
 //
 //  For example, generate(1) should return an empty list and generate(30) should return the numbers: 2,3,5.
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactors {
@@ -14,6 +15,25 @@ public class PrimeFactors {
     }
 
     private static List<Integer> generate(int n) {
-        return null;
+        List<Integer> primeFactors = new ArrayList<>();
+        for (int i = 2; i <= n / 2; i++) {
+            if (n % i == 0 && isPrime(i))
+                primeFactors.add(i);
+        }
+        return primeFactors;
     }
+
+    private static boolean isPrime(int num) {
+        if (num == 2 || num == 1)
+            return true;
+        if (num % 2 == 0)
+            return false;
+        double largestPotentialPrime = Math.round(Math.sqrt(num));
+        for (int i = 3; i <= largestPotentialPrime; i = i + 2) {
+            if (num % i == 0)
+                return false;
+        }
+        return true;
+    }
+
 }
